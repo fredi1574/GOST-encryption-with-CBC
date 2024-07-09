ca0b2356575ac6a47b6266c89cd6112da5453390
Fredi <58234393+fredi1574@users.noreply.github.com>
2024-07-09 12:20:57 +0300

    Update GOST_algorithm_CBC.py

--- GOST_algorithm_CBC.py
+++ GOST_algorithm_CBC.py
@@ -1,3 +1,4 @@
+import os
 
 sBox = [
     [4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3],
@@ -9,3 +10,28 @@ sBox = [
     [13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12],
     [1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12]
 ]
+
+
+# dividing the key into 8 32-bit words
+def divideKey(key):
+    keyWords = []
+    for i in range(0,len(key),4):
+        keyWords.append(int.from_bytes(key[i:i+4], "big"))
+
+    return keyWords
+
+def divideMessage(message):
+    textWords = []
+    for i in range(0,len(message),8):
+        textWords.append(int.from_bytes(message[i:i+8], "big"))
+
+    return textWords
+
+
+message = input("Enter a message you want to encrypt: ").encode() # Input message to be encrypted and encode it
+
+IV = os.urandom(8) # Generate a random 8-byte initialized vector
+divideMessage(message)
+
+
+encryptedMessage = textWords[1] ^ IV
